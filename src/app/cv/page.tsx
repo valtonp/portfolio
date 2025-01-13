55fbb7fee6aab10b85a24efccdd4420a0f484d0e
paul <paul.valton@icloud.com>
2025-01-13 10:03:37 +0100

    modification download

--- src/app/cv/page.tsx
+++ src/app/cv/page.tsx
@@ -1,15 +1,22 @@
-import { Card, CardContent} from "@/components/ui/card"
+import {Card, CardContent, CardHeader} from "@/components/ui/card"
 import Image from "next/image";
+import {Button} from "@/components/ui/button";
 
 export default function CV() {
     return (
         <div className="max-w-4xl mx-auto">
             <Card>
-                <CardContent className={"pt-6"}>
+                <CardHeader>
+                    <Button asChild>
+                        <a href="/CV.png" download>Télécharger mon CV</a>
+                    </Button>
+                </CardHeader>
+                <CardContent>
                     <Image src="/CV.png" alt="CV" height={600} width={400} layout="responsive"/>
                 </CardContent>
             </Card>
         </div>
+
 
     )
 }
