c2f2cced5931df19661a6004e7b8790246f0e8a1
paul <paul.valton@icloud.com>
2025-01-13 09:01:30 +0100

    ajout du CV

--- src/app/cv/page.tsx
+++ src/app/cv/page.tsx
@@ -1,27 +1,16 @@
-import { Button } from "@/components/ui/button"
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
+import { Card, CardContent} from "@/components/ui/card"
+import Image from "next/image";
 
 export default function CV() {
     return (
         <div className="max-w-4xl mx-auto">
             <Card>
-                <CardHeader>
-                    <CardTitle>My CV</CardTitle>
-                    <CardDescription>View or download my curriculum vitae</CardDescription>
-                </CardHeader>
-                <CardContent>
-                    <iframe
-                        src="../../../public/CV.pdf"
-                        className="w-full h-[calc(100vh-300px)] border-none"
-                    />
+                <CardContent className={"pt-6"}>
+                    <Image src="/CV.png" alt="CV" height={600} width={400} layout="responsive"/>
                 </CardContent>
-                <CardFooter>
-                    <Button asChild>
-                        <a href="../../../public/CV.pdf" download>Télécharger CV</a>
-                    </Button>
-                </CardFooter>
             </Card>
         </div>
+
     )
 }
 
