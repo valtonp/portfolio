74366bd5fe681f56bf7a4ae10e7fe4cfd8b5da06
paul <paul.valton@icloud.com>
2025-03-08 15:31:45 +0100

    refactor sidebar.tsx

--- src/components/layout/sidebar.tsx
+++ src/components/layout/sidebar.tsx
@@ -4,6 +4,7 @@ import { Card, CardContent } from "@/components/ui/card"
 import ProfileHeader from "@/components/section/profile-header";
 import MediaButton from "@/components/section/media-button";
 import SkillsList from "@/components/section/skills-list";
+import Languages from "@/components/section/languages";
 
 export default function Sidebar() {
     return (
@@ -39,29 +40,7 @@ export default function Sidebar() {
 
                     <SkillsList />
 
-                    {/* Languages */}
-                    <div className="space-y-3 w-full">
-                        <h2 className="text-lg font-semibold border-b border-gray-200 dark:border-gray-700 pb-2 flex items-center">
-                            Langues
-                        </h2>
-                        <div className="space-y-2">
-                            <div className="flex justify-between items-center">
-                                <span className="text-sm text-gray-600 dark:text-gray-300">Français</span>
-                                <span className="text-xs text-gray-500 dark:text-gray-400">Natif</span>
-                            </div>
-                            <div className="w-full bg-gray-200 dark:bg-gray-700 rounded-full h-1.5">
-                                <div className="bg-primary h-1.5 rounded-full w-full"></div>
-                            </div>
-
-                            <div className="flex justify-between items-center mt-3">
-                                <span className="text-sm text-gray-600 dark:text-gray-300">Anglais</span>
-                                <span className="text-xs text-gray-500 dark:text-gray-400">B1</span>
-                            </div>
-                            <div className="w-full bg-gray-200 dark:bg-gray-700 rounded-full h-1.5">
-                                <div className="bg-primary h-1.5 rounded-full w-[60%]"></div>
-                            </div>
-                        </div>
-                    </div>
+                    <Languages />
                 </div>
             </CardContent>
         </Card>
